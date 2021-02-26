6f05f25b0c222c714d9d08d01f1b59ed843527e7
Heeyeon Koo <rndus0819@naver.com>
2021-02-26 17:43:04 +0900

    보완 많이 필요함..

---
+++ kookoo_cinema/src/components/movie_search/Movie_searchContainer.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Axios from "axios";
+
+
+getSearchMovie=async()=>{
+    const ID_KEY='rVT5ICuUPhyW6ATvcSSa';
+    const SECRET_KEY='GMzcno2UOj';
+    const search=this.state.value;
+    try{
+        if (search===""){
+            console.log("실패했어요ㅠ")
+        }
+        else{
+            const {data:{
+                items
+            }}=await axios.get('https://openapi.naver.com/v1/search/movie.json',{
+                params:{
+                    query:search,
+                    display:20
+                },
+                headers:{
+                    'X-Naver-Client-Id': ID_KEY, 
+                    'X-Naver-Client-Secret': SECRET_KEY
+                }
+            });
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
