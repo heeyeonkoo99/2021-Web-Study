c61b917c71242eff1223c728f917aa81ed206a81
Heeyeon Koo <rndus0819@naver.com>
2021-03-08 23:40:13 +0900

    to be continue..

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,32 @@
 import React, { Fragment,Component } from 'react';
-import EventPractice from "./EventPractice";
+import LifeCycleSample from './LifeCycleSample';
 
-const App=()=>{
-  return <EventPractice/>;
+
+  //랜덤 색상을 생성한다.
+  function getRandomColor() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+
+class App extends Component{
+
+  state={
+    color:"#000000"
+  }
+
+  handleClick=()=>{
+    this.setState({
+      color:getRandomColor()
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    );
+  }
 }
 
 export default App;
