43b0c45da06884d9abe311db57e3c95999cfe402
Heeyeon Koo <rndus0819@naver.com>
2021-03-10 16:28:56 +0900

    리액트 라우터이용하고 카테고리에 NavLink사용+usePromise custom Hook 이용하는 작업도 있음..!

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,16 @@
-import React,{useState} from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-
-const App=()=>{
-  const [data,setData]=useState(null);
-  const onClick=async()=>{
-    try{
-      const response=await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      setData(response.data);
-    }
-    catch(e){
-      console.log(e);
-    };
-  };
-  return(
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data&&<textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
-    </div>
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
-
 };
+
 export default App;
